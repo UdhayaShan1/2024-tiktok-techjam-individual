c552d722470af4dd2540fc65f2ac865d0d46edfb
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-06-30 16:18:08 +0800

    Add nlp section and js script

--- app/static/script.js
+++ app/static/script.js
@@ -15,7 +15,7 @@ document.getElementById('recommendationForm').addEventListener('submit', async f
 
         if (response.ok) {
             const result = await response.json();
-            document.getElementById('recommendationResult').innerText = `Recommended Product: ${result.recommended_product}`;
+            document.getElementById('recommendationResult').innerHTML = `<div class="result-item">Recommended Product: <strong>${result.recommended_product}</strong></div>`;
         } else {
             console.error('Failed to fetch recommendations:', response.statusText);
         }
@@ -23,3 +23,54 @@ document.getElementById('recommendationForm').addEventListener('submit', async f
         console.error('Error fetching recommendations:', error);
     }
 });
+
+document.getElementById('nlpForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const nlpInput = document.getElementById('nlpInput').value;
+
+    try {
+        const response = await fetch('/nlp_predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: nlpInput }),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            document.getElementById('nlpResult').innerHTML = Object.entries(result).map(([key, value]) => 
+                `<div class="result-item"><strong>${key.replace(/_/g, ' ')}:</strong> ${value}</div>`
+            ).join('');
+            
+            await predict(result);
+
+        } else {
+            console.error('Failed to extract circumstances:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error extracting circumstances:', error);
+    }
+});
+
+async function predict(jsonResult) {
+    try {
+        const response = await fetch('/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jsonResult),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            document.getElementById('nlpResult2').innerHTML = `<div class="result-item">Recommended Product: <strong>${result.recommended_product}</strong></div>`;
+        } else {
+            console.error('Failed to fetch product recommendation:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error fetching product recommendation:', error);
+    }
+}
